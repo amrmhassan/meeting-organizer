ef9b910043351315fd22fdfc046fe0659ee77eeb
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2022-10-11 09:04:59 +0200

    test comment

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // this is a test comment
       home: SignInScreen(),
     );
   }
